fe11e7c2b1d09ccd704dbf4fb980e3aa0912f0da
V1A0 <54343363+V1A0@users.noreply.github.com>
2021-06-18 16:39:44 +0700

    wf upd

--- tests/temp_tests.py
+++ tests/temp_tests.py
@@ -1,26 +1,23 @@
-from sqllex import *
+...
 
-# Create some database, with simple structure
-db = SQLite3x(                              # create database
-    path='my_data.db',                      # __str__ to your database, or where_ you would like it locate
-    template={                              # schema for tables inside your database
-        "users": {                          # name for the 1'st table
-            "username": [TEXT, NOT_NULL],   # 1'st column of table, named "username", contains text-data, can't be NULL
-            "age": INTEGER,                 # 2'nd column of table, named "age", contains integer value
-        }                                   # end of table
-    }                                       # end of schema (template)
+users = db['users']    # Get table from database as object
+
+id_col = users['id']    # Get table from database as object
+
+name_col = users['name']    # Get another column from table as object
+
+
+users.insert([1, 'Alex'])
+users.insert([2, 'Blex'])
+
+...
+
+users.update(
+        {
+            id_col: id_col + 2
+        },
+        WHERE=(name_col == 'Alex')
 )
 
-# Ok, now you have database with table inside it.
-# Let's take this table as variable
-users = db["users"]
 
-# Now add record of 33 years old user named 'Sqllex' into it
-# Dear table, please insert ['Sqllex', 33] values
-users.insert('Sqllex', 33)
-
-# Dear table, please find records where_ column 'age' == 33
-user = users.find(age=33)
-
-# Print results
-print(user)  # [['Sqllex', 33]]
+users.select([name_col, id_col], WHERE=(id_col == 3))   # [['Alex', 3]]
