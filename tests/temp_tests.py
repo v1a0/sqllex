1870d13a9cce312a65227feb7c45cbb9e23c92fb
V1A0 <54343363+V1A0@users.noreply.github.com>
2021-06-19 10:00:54 +0700

    public classes sqllex.classes
    - add to public classes sqllex.classes SQLite3xColumn, SQLite3xSearchCondition

--- tests/temp_tests.py
+++ tests/temp_tests.py
@@ -1,23 +1,5 @@
-...
+from sqllex.core.entities.sqlite3x.sqlite3x import SQLite3xColumn
 
-users = db['users']    # Get table from database as object
+num = SQLite3xColumn("users", "num")
 
-id_col = users['id']    # Get table from database as object
-
-name_col = users['name']    # Get another column from table as object
-
-
-users.insert([1, 'Alex'])
-users.insert([2, 'Blex'])
-
-...
-
-users.update(
-        {
-            id_col: id_col + 2
-        },
-        WHERE=(name_col == 'Alex')
-)
-
-
-users.select([name_col, id_col], WHERE=(id_col == 3))   # [['Alex', 3]]
+num = sqllex.core.entities.sqlite3x.sqlite3x.SQLite3xColumn("users", "num")
